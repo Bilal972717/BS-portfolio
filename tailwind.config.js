cf20bdb8930ef0f7baca920feae93ff5c9f592f1
Bilal972717 <bilalsherwanirbc@gmail.com>
2026-03-10 01:11:06 +0500

    Fix: Added postcss.config.js to enable Tailwind compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 10s linear infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
